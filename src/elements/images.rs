087cb145d11282124555d9f03ce192c280cecc5b
Robin Krahl <robin.krahl@ireas.org>
2021-06-15 14:37:29 +0200

    Fix and simplify bounding box calculation
    This patch simplifies the calculation of the bounding box for an image
    and thereby fixes a bug where the size of the bounding box would not be
    calculated correctly for rotations in the range [-90..0].

--- src/elements/images.rs
+++ src/elements/images.rs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2020 Alexander Dean-Kennedy <dstar@slackless.com>
+// SPDX-FileCopyrightText: 2021 Robin Krahl <robin.krahl@ireas.org>
 // SPDX-License-Identifier: Apache-2.0 or MIT
 
 //! Image support for genpdf-rs.
@@ -233,50 +234,48 @@ impl Element for Image {
     }
 }
 
-/// Given the Size of a box (width/height), compute the bounding-box size when
-/// rotated some degrees and where the "minimum" corner is (which should be the
-/// new origin/offset). Note, this is not very optimized.
+/// Given the Size of a box (width/height), compute the bounding-box size and offset when
+/// rotated some degrees.  The offset is the distance from the top-left corner of the bounding box
+/// to the (originally) lower-left corner of the image.
 #[allow(clippy::manual_range_contains)]
 fn bounding_box_offset_and_size(rotation: &Rotation, size: &Size) -> (Position, Size) {
-    let theta = rotation.degrees.to_radians();
-    let (ct, st) = (theta.cos(), theta.sin());
-    let (w, h): (f64, f64) = (size.width.into(), size.height.into());
-    match rotation.degrees {
-        d if d > 0.0 && d <= 90.0 => {
-            let alpha = 180.0 - (rotation.degrees + 90.0);
-            let ca = alpha.to_radians().cos();
-            let (hct, wct) = (h * ct, w * ct);
-            let (hst, wst) = (h * st, w * st);
-            let (bb_w, bb_h) = (hst + wct, wst + hct);
-            (Position::new(h * ca, bb_h), Size::new(bb_w, bb_h))
-        }
-        d if d > 90.0 && d <= 180.0 => {
-            let alpha = (rotation.degrees - 90.0).to_radians();
-            let (ca, sa) = (alpha.cos(), alpha.sin());
-            let (bb_w, bb_h) = (w * sa + h * ca, w * ca + h * sa);
-            (Position::new(bb_w, w * ca), Size::new(bb_w, bb_h))
-        }
-        d if d < 0.0 && d > -90.0 => {
-            let (hct, wct) = (h * ct, w * ct);
-            let (hst, wst) = (h * st, w * st);
-            let (bb_w, bb_h) = (hst + wct, hct + wst);
-            (Position::new(0, hct), Size::new(bb_w, bb_h))
-        }
-        d if d <= -90.0 && d >= -180.0 => {
-            let alpha = (180.0 + rotation.degrees).to_radians();
-            let (ca, sa) = (alpha.cos(), alpha.sin());
-            let (bb_w, bb_h) = (h * sa + w * ca, h * ca + w * sa);
-            (Position::new(w * ca, 0), Size::new(bb_w, bb_h))
-        }
-        _ =>
-        // This section is only for degrees == 0.0, but I use the default match due to:
-        //       https://github.com/rust-lang/rust/issues/41620
-        // Rotation's degrees should be restricted to [-180,180] so these
-        // ranges should be complete.
-        {
-            (Position::new(0, h), *size)
-        }
-    }
+    // alpha = rotation, beta = 90 - rotation
+    let alpha = rotation.degrees.to_radians();
+    let beta = (90.0 - rotation.degrees).to_radians();
+
+    // s* = sin of *
+    let sa = alpha.sin();
+    let sb = beta.sin();
+
+    // Bounding box calculation, based on
+    // https://math.stackexchange.com/questions/1628657/dimensions-of-a-rectangle-containing-a-rotated-rectangle
+    let width = (size.width.0 * sb).abs() + (size.height.0 * sa).abs();
+    let height = (size.height.0 * sb).abs() + (size.width.0 * sa).abs();
+    let bb_size = Size::new(width, height);
+
+    // Offset calculation -- to follow the calculations, consider the rotated rectangles, their
+    // bounding boxes and the triangles between them
+    let bb_position = if rotation.degrees < -180.0 {
+        unreachable!(
+            "Rotations must be in the range -180.0..=180.0, but got: {}",
+            rotation.degrees
+        );
+    } else if rotation.degrees <= -90.0 {
+        Position::new(size.width.0 * alpha.cos().abs(), 0)
+    } else if rotation.degrees <= 0.0 {
+        Position::new(0, size.height.0 * alpha.cos())
+    } else if rotation.degrees <= 90.0 {
+        Position::new(size.height.0 * beta.cos(), bb_size.height.0)
+    } else if rotation.degrees <= 180.0 {
+        Position::new(bb_size.width.0, size.width.0 * beta.cos())
+    } else {
+        unreachable!(
+            "Rotations must be in the range -180.0..=180.0, but got: {}",
+            rotation.degrees
+        );
+    };
+
+    (bb_position, bb_size)
 }
 
 #[cfg(test)]
@@ -285,7 +284,6 @@ mod tests {
     use crate::{Position, Rotation, Size};
     use float_cmp::approx_eq;
 
-    // TODO: reduce EPSILON and ulps
     macro_rules! assert_approx_eq {
         ($typ:ty, $lhs:expr, $rhs:expr) => {
             let left = $lhs;
@@ -326,8 +324,7 @@ mod tests {
         let size = Size::new(100, 100);
         let bb_width = (60.0f64.to_radians().sin() + 30.0f64.to_radians().sin()) * size.width.0;
         let bb_size = Size::new(bb_width, bb_width);
-        // TODO: -30, -60 fails
-        for rotation in &[-150.0, -120.0, 30.0, 60.0, 120.0, 150.0] {
+        for rotation in &[-150.0, -120.0, -30.0, -60.0, 30.0, 60.0, 120.0, 150.0] {
             println!("rotation = {}", rotation);
             let rotation = Rotation::from(*rotation);
             assert_approx_eq!(
@@ -343,8 +340,7 @@ mod tests {
         let size = Size::new(100, 100);
         let bb_width = (2.0f64 * size.width.0.powf(2.0)).sqrt();
         let bb_size = Size::new(bb_width, bb_width);
-        // TODO: -45 fails
-        for rotation in &[-135.0, 45.0, 135.0] {
+        for rotation in &[-135.0, -45.0, 45.0, 135.0] {
             println!("rotation = {}", rotation);
             let rotation = Rotation::from(*rotation);
             assert_approx_eq!(
@@ -413,8 +409,7 @@ mod tests {
         let bb_height =
             60.0f64.to_radians().sin() * size.height.0 + 30.0f64.to_radians().sin() * size.width.0;
         let bb_size = Size::new(bb_width, bb_height);
-        // TODO: -30 fails
-        for rotation in &[-150.0, 30.0, 150.0] {
+        for rotation in &[-150.0, -30.0, 30.0, 150.0] {
             println!("rotation = {}", rotation);
             let rotation = Rotation::from(*rotation);
             assert_approx_eq!(
@@ -430,8 +425,7 @@ mod tests {
         let size = Size::new(200, 100);
         let bb_width = 45.0f64.to_radians().sin() * (size.width.0 + size.height.0);
         let bb_size = Size::new(bb_width, bb_width);
-        // TODO: -45 fails
-        for rotation in &[-135.0, 45.0, 135.0] {
+        for rotation in &[-135.0, -45.0, 45.0, 135.0] {
             println!("rotation = {}", rotation);
             let rotation = Rotation::from(*rotation);
             assert_approx_eq!(
@@ -450,8 +444,7 @@ mod tests {
         let bb_height =
             30.0f64.to_radians().sin() * size.height.0 + 60.0f64.to_radians().sin() * size.width.0;
         let bb_size = Size::new(bb_width, bb_height);
-        // TODO: -60 fails
-        for rotation in &[-120.0, 60.0, 120.0] {
+        for rotation in &[-120.0, -60.0, 60.0, 120.0] {
             println!("rotation = {}", rotation);
             let rotation = Rotation::from(*rotation);
             assert_approx_eq!(
